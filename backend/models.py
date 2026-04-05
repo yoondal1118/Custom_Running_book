e7dda88a36ecd432dd11941fa02b6d7e3024c002
jinyoung Lee <prin46@naver.com>
2026-04-05 15:32:12 +0900

    DB 휴대폰 필수 기재사항으로 변경

--- backend/models.py
+++ backend/models.py
@@ -11,7 +11,7 @@ class User(Base):
     name             = Column(String, nullable=False)
     email            = Column(String, unique=True, index=True, nullable=False)
     hashed_password  = Column(String, nullable=False)
-    phone            = Column(String, nullable=True)
+    phone            = Column(String, nullable=False)
     created_at       = Column(DateTime(timezone=True), server_default=func.now())
 
     addresses = relationship("Address", back_populates="user", cascade="all, delete-orphan")
